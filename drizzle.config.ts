021cbd4e259f93fc42a14639f7d862bc749f183c
zonggonau <cristoperzonggonau@gmail.com>
2026-01-23 15:27:04 +0900

    fix: db-migrate service target and config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
+dotenv.config();
+
 
 export default defineConfig({
     out: "./drizzle",
